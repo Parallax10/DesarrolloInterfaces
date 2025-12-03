14618f8bb93120959bb22f026e4b211765e5e2e3
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2025-12-03 14:44:14 +0100

    1

--- tareas/Tema2/estilos2/src/app/page.js
+++ tareas/Tema2/estilos2/src/app/page.js
@@ -3,64 +3,34 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <div className="header">
+        <div>
+          <img src="popcat.png" height={100} width={130}></img>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div>
+          <input></input>
+          <button>Buscar</button>
         </div>
-      </main>
+        <div>
+          <button>Registrate</button>
+          <button>Inicia Sesion</button>
+        </div>
+      </div>
+      <div className="grid">
+        <div className="g1">
+          <img src="gato.jpg" height={500} width={550}></img>
+        </div>
+        <div className="g2"> 
+          <p>gato que te mira asin</p>
+        </div>
+      
+      </div>
+      <div>
+        <footer className="foot">
+          <p>Solo un gato que te mira asin</p>
+        </footer>
+      </div>
     </div>
   );
 }
