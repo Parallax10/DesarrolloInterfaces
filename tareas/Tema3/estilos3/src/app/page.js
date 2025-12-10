e537bfa76568103c7d93bf87e1d7a597d0011f7b
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2025-12-10 14:01:47 +0100

    1

---
+++ tareas/Tema3/estilos3/src/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="divGrande">
+      <div className="cuadrao">
+
+      </div>
+      <div className="cuadra2">
+
+      </div>
+      <div className="cuadra3">
+
+      </div>
+    </div>
+  );
+}
