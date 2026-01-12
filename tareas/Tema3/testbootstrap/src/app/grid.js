91b0740d97ade07f8eec56e9916d1aab3c9e1b30
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2026-01-12 12:30:20 +0100

    1

---
+++ tareas/Tema3/testbootstrap/src/app/grid.js
@@ -0,0 +1,15 @@
+export default function Grid() {
+return (
+    <div className="container">
+        <h2 className="text-center">grid ej1</h2>
+        <div className="row">
+        <div className="col-md-4">Columna 1 </div>
+        <div className="col-md-4">Columna 2 </div>
+        <div className="col-md-4">Columna 3 </div>
+        <div className="row"></div>
+        <div className="col-md-8">Columna 1 </div>
+        <div className="col-md-4">Columna 2 </div>
+    </div>
+    </div>
+);
+}
--- tareas/Tema3/testbootstrap/src/app/page.js
+++ tareas/Tema3/testbootstrap/src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Grid from "./grid";
+
 
 export default function Home() {
   return (
     <div>
-      
+      <h1>EJ1</h1>
+      <Grid></Grid>
     </div>
   );
 }
